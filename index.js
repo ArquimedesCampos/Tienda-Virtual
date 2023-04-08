58ade16c20797134208d01d8d1c083982912f17d
unknown <angelcamposmarin97@gmail.com>
2023-04-07 22:49:19 -0400

    primera version

---
+++ index.js
@@ -0,0 +1,7 @@
+const btnCart=document.querySelector('.container-icon')
+
+const containerCartProduct=document.querySelector('.container-cart-products')
+
+btnCart.addEventListener('click', ()=>{
+    containerCartProduct.classList.toggle('hiddem-cart')
+})
